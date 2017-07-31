f81e790e928f859b8afdf51fd32882adb0022918
Sana Diwan <sanadiwan2006@gmail.com>
2017-07-31 14:31:20 +0000

    cleaning up further

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 //animating links bar on scroll
+//source: think123 (2017). How do I make a <div> move up and down when I'm scrolling the page?. [online] Stackoverflow.com. Available at: https://stackoverflow.com/questions/1638895/how-do-i-make-a-div-move-up-and-down-when-im-scrolling-the-page [Accessed 31 Jul. 2017].
 $(window).scroll(function(){
         $(".links").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "fast" );
         });
