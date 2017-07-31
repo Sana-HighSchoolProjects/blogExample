890a969c509834d69d9bc34dfaf4b927e97e1aeb
Sana Diwan <sanadiwan2006@gmail.com>
2017-07-31 14:16:40 +0000

    adding comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+//testing if js is working
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Ready to roll!");
 }, false);
+
+//animating links bar on scroll
+$(window).scroll(function(){
+        $(".links").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "fast" );
+        });
